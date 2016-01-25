d8309505e1a8af4457177a6aee3388678da41a42
Student <student@RU-023.local>
2016-01-25 15:42:42 -0700

    first commit. Need to finish #2,#6 and #10

---
+++ main.js
@@ -0,0 +1,73 @@
+// #1
+var tripleFive = function (i){
+	for (i = 1; i <= 3; i++) {
+		console.log("Five!")
+	}
+}
+// #2 NEED TO FINISH
+var lastLetter = function() {
+
+}
+
+
+// #3
+var square = function (number){
+	number = number * number
+	console.log(number)
+}
+
+// #4
+var negate = function (number){
+	number = number * -1
+	console.log(number)
+}
+
+// #5 I want to develop a loop to cover nunbers1-3 cover in code review
+var toArray = function (number1, number2, number3) {
+	// for (i = 1; i < 4; i++) {
+		var myArray = []
+		number1 = myArray.push(number1)
+		number2 = myArray.push(number2)
+		number3 = myArray.push(number3)
+		console.log(myArray)
+	// }
+}
+
+// #6 NEED TO FINISH
+var startsWithA = function(string){
+	if (string.length[0] = "A" || "a") {
+		console.log(true)
+	}
+	else {
+		console.log(false)
+	}
+}
+
+// #7
+var excite = function(x){
+	x = x + "!!!"
+	console.log(x)
+}
+
+// #8
+var sun = function(sun){
+
+}
+
+// #9
+var tiny = function(number){
+	if (number < 0) {
+		console.log(false)
+	}
+	else if (number < 1) {
+		console.log(true)
+	}
+	else {
+		console.log(false)
+	}
+}
+
+// #10
+// var getSeconds = function(MM:SS) {
+
+// }
