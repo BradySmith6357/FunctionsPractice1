01bd3b3fa83a6b4b2c56bf9066674db33492d2be
Student <student@RU-023.local>
2016-01-28 14:45:06 -0700

    Worked on FunctionsPart2

--- main.js
+++ main.js
@@ -1,73 +1,186 @@
-// #1
-var tripleFive = function (i){
-	for (i = 1; i <= 3; i++) {
-		console.log("Five!")
-	}
-}
-// #2 NEED TO FINISH
-var lastLetter = function() {
+// // #1
+// var tripleFive = function (i){
+// 	for (i = 1; i <= 3; i++) {
+// 		console.log("Five!")
+// 	}
+// }
+// // #2 NEED TO FINISH
+// var lastLetter = function() {
 
-}
+// }
 
 
-// #3
-var square = function (number){
-	number = number * number
-	console.log(number)
-}
+// // #3
+// var square = function (number){
+// 	number = number * number
+// 	console.log(number)
+// }
 
-// #4
-var negate = function (number){
-	number = number * -1
-	console.log(number)
-}
+// // #4
+// var negate = function (number){
+// 	number = number * -1
+// 	console.log(number)
+// }
 
-// #5 I want to develop a loop to cover nunbers1-3 cover in code review
-var toArray = function (number1, number2, number3) {
-	// for (i = 1; i < 4; i++) {
-		var myArray = []
-		number1 = myArray.push(number1)
-		number2 = myArray.push(number2)
-		number3 = myArray.push(number3)
-		console.log(myArray)
-	// }
-}
+// // #5 I want to develop a loop to cover nunbers1-3 cover in code review
+// var toArray = function (number1, number2, number3) {
+// 	// for (i = 1; i < 4; i++) {
+// 		var myArray = []
+// 		number1 = myArray.push(number1)
+// 		number2 = myArray.push(number2)
+// 		number3 = myArray.push(number3)
+// 		console.log(myArray)
+// 	// }
+// }
 
-// #6 NEED TO FINISH
-var startsWithA = function(string){
-	if (string.length[0] = "A" || "a") {
-		console.log(true)
-	}
-	else {
-		console.log(false)
-	}
-}
+// // #6 NEED TO FINISH
+// var startsWithA = function(string){
+// 	if (string.length[0] = "A" || "a") {
+// 		console.log(true)
+// 	}
+// 	else {
+// 		console.log(false)
+// 	}
+// }
 
-// #7
-var excite = function(x){
-	x = x + "!!!"
-	console.log(x)
-}
+// // #7
+// var excite = function(x){
+// 	x = x + "!!!"
+// 	console.log(x)
+// }
 
-// #8
-var sun = function(sun){
+// // #8
+// var sun = function(sun){
 
-}
+// }
 
-// #9
-var tiny = function(number){
-	if (number < 0) {
-		console.log(false)
-	}
-	else if (number < 1) {
-		console.log(true)
-	}
-	else {
-		console.log(false)
-	}
-}
+// // #9
+// var tiny = function(number){
+// 	if (number < 0) {
+// 		console.log(false)
+// 	}
+// 	else if (number < 1) {
+// 		console.log(true)
+// 	}
+// 	else {
+// 		console.log(false)
+// 	}
+// }
 
 // #10
 // var getSeconds = function(MM:SS) {
 
 // }
+
+
+
+// Function Practice #2
+
+// // #1
+// var getName = function(object){
+// 	return object.name
+// }
+
+// console.log(getName({name: "Luisa", age: 25}))
+
+// // #2 
+// var totalLetters = function(array) {
+// 	var count = 0
+// 	for (i = 0; i < array.length; i++){
+// 		count += array[i].length
+// 	}
+// 	return count
+// }
+// console.log(totalLetters(['javascript', 'is', 'awesome']))
+
+
+// // #3 NOT RETURNING key variable
+// var keyValue = function(key, variable) {
+// 	var object1 = {}
+// 		object1[key] = variable
+// 		return object1
+// 	}
+// console.log(keyValue("city", "Denver"))
+
+// #4
+// var negativeIndex = function(array, negnum) {
+// 	return array[array.length + negnum] 
+// }
+// console.log(negativeIndex(['a', 'b', 'c', 'd', 'e'], -2))
+
+// #5 RETURNING UNDEFINED
+// var removeM = function(str) {
+// 	var newstr = ""
+// 	for(var i = 0; i < str.length; i++){
+// 		if (str.length[i] != "m") {
+// 			str[i] += newstr
+// 		}
+// 	}
+// 	return newstr
+// }
+// console.log(removeM("family"))
+// #6
+// var printObject = function(array){
+// 		array.key = key
+// 		array.value = value
+// 		console.log(key + " is " + value)
+// }
+// printObject({ a: 10, b: 20, c: 30 })
+
+// #7 RETURNING UNDEFINED
+// var vowels = function(str) {
+// 	var arr = []
+// 	var object = {
+// 		vowel:"a",
+// 		vowel:"e",
+// 		vowel:"i",
+// 		vowel:"o",
+// 		vowel:"u"
+// 	}
+// 	for (i=0; i < str.length; i++) {
+// 		if (str.length[i] === object.vowel) {
+// 			arr.push(str.length[i])
+// 		}
+// 	}
+// }
+// console.log(vowels('alabama'))
+
+// #8 
+var twins = function()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
